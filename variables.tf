b7b0321b7a656558cbc4c18d26e06b0f93d85b5f
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:56:27 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ EOT
     resource_group_name          = string
     servicebus_namespace         = string
     stream_analytics_job_name    = string
-    authentication_mode          = optional(string, "ConnectionString")
+    authentication_mode          = optional(string) # Default: "ConnectionString"
     eventhub_consumer_group_name = optional(string)
     partition_key                = optional(string)
     shared_access_policy_key     = optional(string)
